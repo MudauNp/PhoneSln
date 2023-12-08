1d084987faac4dd3151f45135cb494462e2db436
MudauNP <5100154@mypillar7uwc.ac.za>
2023-12-08 16:15:37 +0200

    Add project files.

---
+++ PhoneSln/Mobile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneSln
+{
+    internal class Mobile
+    {
+        private string Brand;
+        private string Display;
+        private string Storage;
+        private bool Hasinsurance;
+        
+        public Mobile(string brand,string display, string storage, bool hasinsurance)
+        {
+            Brand = brand;
+            Display = display;
+            Storage = storage;
+            Hasinsurance = hasinsurance;
+                     
+        
+        }
+
+        public string GetBrand()
+        {
+         return Brand;
+
+        }
+         
+        public string GetDisplay()
+        {
+            return Display;
+        }
+        public string GetStorage()
+        {
+            return Storage;
+        }
+        public bool GetHasinsurance()
+        { 
+            return Hasinsurance; 
+        }
+        
+
+
+
+
+
+    }   
+}
---
+++ PhoneSln/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PhoneSln
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile MyMobile = new Mobile("Iphone 15 Pro", "Super Retina XDR display", "512 GB", true);
+
+            Console.WriteLine("What is the brand of the mobile? " + MyMobile.GetBrand());
+            Console.WriteLine("What is the display of the mobile? " + MyMobile.GetDisplay());
+            Console.WriteLine("What is the storage of the mobile? " + MyMobile.GetStorage());
+            if (MyMobile.GetHasinsurance()) 
+            {
+                Console.WriteLine("HasInsurance : Yes");
+            }
+
+            else
+            {
+                Console.WriteLine("HasInsurance : No");
+            }
+        }
+    }
+}
